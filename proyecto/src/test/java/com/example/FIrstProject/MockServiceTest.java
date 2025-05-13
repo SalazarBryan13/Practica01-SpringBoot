24cf1cc453eef980dbb31c5aa9ba738e138cdbeb
Bryan Salazar <bryan.salazar02@epn.edu.ec>
2025-05-12 20:38:24 -0500

    test: Prueba unitaria para el servicio de saludo (MockServiceTest)

--- proyecto/src/test/java/com/example/FIrstProject/MockServiceTest.java
+++ proyecto/src/test/java/com/example/FIrstProject/MockServiceTest.java
@@ -1,4 +1,5 @@
 package com.example.FIrstProject;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
